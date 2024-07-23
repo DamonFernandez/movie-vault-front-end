3c09e262f1e0f4ef34e90ac5f0d97410648d64a7
Blitzcranq <vrajchauhan2110@gmail.com>
2024-07-22 20:19:05 -0400

    fix/error movies not returning properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { useState } from "react";
-
 import Movies from "./routes/movies";
 
 import "./styles/App.css";
 
 function App() {
-  return <>
-    <h1>MovieVault</h1>
-    <Movies />
-  </>;
+  return (
+    <>
+      <h1>MovieVault</h1>
+      <Movies />
+    </>
+  );
 }
 
 export default App;
